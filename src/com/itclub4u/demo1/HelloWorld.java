a81da4caf93cb095004cc895e550e9bc20168d28
406046602 <406046602@qq.com>
2017-08-15 10:26:52 +0800

    Initial commit

---
+++ src/com/itclub4u/demo1/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.itclub4u.demo1;
+
+/**
+ * Created by 陈才 on 2017/8/15.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloWorld");
+    }
+}
